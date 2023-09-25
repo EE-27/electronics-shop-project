b0c55072e2ca9bb1b6cc78c863362d0bd4eba1fe
JL34 <johnny2791@seznam.cz>
2023-09-25 09:24:06 +0200

    hw4 made phone.py, class Phone, main works(at the end of hw-4/main.py is raise ValueError)

---
+++ src/phone.py
@@ -0,0 +1,27 @@
+from item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(\'{self.name}\', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+    def __add__(self, other):
+        return self.quantity + other.quantity
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, x):
+        if x <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.__number_of_sim = x
